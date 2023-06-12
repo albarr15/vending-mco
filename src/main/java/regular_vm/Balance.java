4eb8771d735bd267f0975aed66a7bb6dc62bf7b6
albarr15 <cmalbarracin15@gmail.com>
2023-06-12 21:50:25 +0800

    Created skeleton code for Balance class

--- src/main/java/regular_vm/Balance.java
+++ src/main/java/regular_vm/Balance.java
@@ -1,4 +1,21 @@
 package regular_vm;
 
 public class Balance {
+    private double currentBal;
+
+    public void increaseBal(double incBal) {
+
+    }
+
+    public void decreaseBal(double decBal) {
+
+    }
+
+    public void setCurrentBal(double newBal) {
+
+    }
+
+    public double getCurrentBal() {
+        return currentBal;
+    }
 }
