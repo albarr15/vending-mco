f5fe926dcc4c1b5f0869c72cff499e71b93b1891
albarr15 <cmalbarracin15@gmail.com>
2023-06-12 16:19:47 +0800

    Created skeleton code for RegularVM class

--- src/main/java/regular_vm/RegularVM.java
+++ src/main/java/regular_vm/RegularVM.java
@@ -1,4 +1,28 @@
 package regular_vm;
+import java.util.ArrayList;
 
 public class RegularVM {
+    private ArrayList<ItemSlot> listItemSlots;
+    private Balance balance;
+    private Transaction currentTransaction;
+
+    public void addItemSlot(ItemSlot itemSlot) {
+
+    }
+
+    public void displaySlotAvailability(ItemSlot itemSlot) {
+
+    }
+
+    public void displayAllSlots() {
+
+    }
+
+    public void makeTransaction() {
+
+    }
+
+    public void receivePayment(double payment) {
+
+    }
 }
