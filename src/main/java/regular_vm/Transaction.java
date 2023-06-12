cdeccbbe5316648e30e4e7e4bc9c87bfc2ea1ed6
albarr15 <cmalbarracin15@gmail.com>
2023-06-12 21:48:20 +0800

    Created skeleton code for Transaction class

--- src/main/java/regular_vm/Transaction.java
+++ src/main/java/regular_vm/Transaction.java
@@ -1,4 +1,34 @@
 package regular_vm;
+import java.util.ArrayList;
 
 public class Transaction {
+    private ArrayList<Item> currentCart;
+    private double orderTotal;
+    private double amtReceived;
+    private boolean status;
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void addToCart(Item item) {
+
+    }
+
+    public void removeFromCart(Item item) {
+
+    }
+
+    public void receiveMoney(double amount) {
+
+    }
+
+    public double dispenseChange() {
+        // to be modified
+        return 0;
+    }
 }
