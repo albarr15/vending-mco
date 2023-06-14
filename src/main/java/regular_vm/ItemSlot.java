c0ea6cde3b9f5e851dea4483d215cd164b6efe08
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-06-14 21:24:47 +0800

    Initial code for ItemSlot.java

--- src/main/java/regular_vm/ItemSlot.java
+++ src/main/java/regular_vm/ItemSlot.java
@@ -1,4 +1,45 @@
 package regular_vm;
+import java.util.ArrayList;
 
 public class ItemSlot {
+    Item item = null;
+    ArrayList<Item> listItem = new ArrayList<Item>();
+    double price;
+
+    public void setItem(Item item) {
+        this.item = item;
+        listItem.clear();
+    }
+    public String getItem() {
+        return this.item.getName();
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+
+    public boolean checkSlotAvailability() {
+        if(this.item == null)
+            return true;
+        else return false;
+    }
+
+    public boolean dispenseItem() {
+        if(listItem.size() > 0) {
+            listItem.remove(listItem.size()-1); //must destroy
+            return true;
+        } else return false;
+    }
+
+    public boolean stockItem() {
+        if(listItem.size() < 10) {
+            Item item = new Item(this.item.getName(), 
+                            this.item.getCaloriesAmt());
+            listItem.add(item);
+            return true;
+        } else return false;
+    }
 }
