4bd67a724e830c87c8f95dcbab739d5db20812a9
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-06-14 20:50:27 +0800

    Update Item.java

--- src/main/java/regular_vm/Item.java
+++ src/main/java/regular_vm/Item.java
@@ -1,4 +1,25 @@
 package regular_vm;
 
 public class Item {
+    String name;
+    double caloriesAmt;
+
+    public Item(String name, double caloriesAmt) {
+        this.name = name;
+        this.caloriesAmt = caloriesAmt;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCaloriesAmt(double caloriesAmt) {
+        this.caloriesAmt = caloriesAmt;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public double getCaloriesAmt() {
+        return this.caloriesAmt;
+    }
 }
