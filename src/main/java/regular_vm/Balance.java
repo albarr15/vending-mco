19cb071dfd92992dc6ce910ea736190d9519e6c0
albarr15 <cmalbarracin15@gmail.com>
2023-06-15 13:20:08 +0800

    Updated initial code for Balance.java

--- src/main/java/regular_vm/Balance.java
+++ src/main/java/regular_vm/Balance.java
@@ -4,18 +4,18 @@ public class Balance {
     private double currentBal;
 
     public void increaseBal(double incBal) {
-
+        this.currentBal = currentBal + incBal;
     }
 
     public void decreaseBal(double decBal) {
-
+        this.currentBal = currentBal - decBal;
     }
 
     public void setCurrentBal(double newBal) {
-
+        this.currentBal = newBal;
     }
 
     public double getCurrentBal() {
-        return currentBal;
+        return this.currentBal;
     }
 }
