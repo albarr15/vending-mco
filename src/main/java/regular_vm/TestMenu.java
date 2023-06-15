e34cee44e7a352b5020c8a6842a3f4cbe141a015
albarr15 <cmalbarracin15@gmail.com>
2023-06-15 13:47:47 +0800

    Created skeleton code for TestMenu.java

--- src/main/java/regular_vm/TestMenu.java
+++ src/main/java/regular_vm/TestMenu.java
@@ -1,4 +1,33 @@
 package regular_vm;
 
 public class TestMenu {
+
+    public void setItem(int ItemIndex) {
+
+    }
+
+    public void stockItem(int ItemIndex, int noItems) {
+
+    }
+
+    public void setPrice(int ItemIndex, double price) {
+
+    }
+
+    public double collectMoney(double amount) {
+        return 0;
+    }
+
+    public double collectMoney() {
+        return 0;
+    }
+
+    public void replenishMoney(double amount) {
+
+    }
+
+    public void printTransacSummary() {
+
+    }
+
 }
