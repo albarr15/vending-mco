900dd711f7dd22ca33b2fa783ff6cd13b13c8cc3
albarr15 <cmalbarracin15@gmail.com>
2023-06-19 18:39:13 +0800

    Finished itemSlot-related methods in RegularVM.java

--- src/main/java/regular_vm/RegularVM.java
+++ src/main/java/regular_vm/RegularVM.java
@@ -7,15 +7,30 @@ public class RegularVM {
     private Transaction currentTransaction;
 
     public void addItemSlot(ItemSlot itemSlot) {
-
+        listItemSlots.add(itemSlot);
     }
 
     public void displaySlotAvailability(ItemSlot itemSlot) {
-
+        if (itemSlot.checkSlotAvailability()) {
+            System.out.println("Slot with " + itemSlot.item.name + "items is NOT AVAILABLE.");
+        }
+        else {
+            System.out.println("Slot with " + itemSlot.item.name + "items is AVAILABLE.");
+        }
     }
 
     public void displayAllSlots() {
+        int size = listItemSlots.size();
 
+        // prints each itemSlot with its corresponding stocked item
+        for (int i = 0; i < size; i++) {
+            if ((listItemSlots.get(i)).checkSlotAvailability()) {
+                System.out.println("Slot " + i + " ( OUT OF STOCK )");
+            }
+            else {
+                System.out.println("Slot " + i + " (" + (listItemSlots.get(i)).item.name + ")");
+            }
+        }
     }
 
     public void makeTransaction() {
