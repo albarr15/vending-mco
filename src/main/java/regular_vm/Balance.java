555ecd797b967f1873d3d7c0eba6dc252369bae8
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-06-23 23:17:47 +0800

    Allowed storing / withdrawing specific denominations of cash for Balance.java

--- src/main/java/regular_vm/Balance.java
+++ src/main/java/regular_vm/Balance.java
@@ -1,24 +1,60 @@
 package regular_vm;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Balance {
-    private double currentBal;
-    private ArrayList<Double> listDenominations = new ArrayList<Double>();
+    private double currentBal = 0;
+    private double[][] cashStock = { {0.01,0.05,0.1,0.25,0.5,1,5,10,20,50,100,200,500,1000},
+                                     {0,0,0,0,0,0,0,0,0,0,0,0,0,0} };
 
-    public Balance (ArrayList<Double> listDenominations1) {
-        this.listDenominations.addAll(listDenominations1);
+    public Balance () {}
+
+    public double[][] getCashStock() {
+        return this.cashStock;
     }
 
-    public ArrayList<Double> getListDenominations() {
-        return this.listDenominations;
+    // Receives string input containing the different denominations
+    public void depositCash(String cashList) {
+        // Extract cash values from string input
+        ArrayList<Double> cash = new ArrayList<Double>();
+        StringTokenizer st = new StringTokenizer(cashList," ");
+        while(st.hasMoreTokens()) {
+            cash.add(Double.parseDouble(st.nextToken()));
+        }
+
+        // Store in balance
+        for(int i=0; i<cash.size(); i++)
+            for(int j=0; j<14; j++)
+                if(cash.get(i) == this.cashStock[0][j]) {
+                    this.cashStock[1][j]++;
+                    this.currentBal += cash.get(i);
+                    break;
+                }
     }
 
-    public void increaseBal(double incBal) {
-        this.currentBal = currentBal + incBal;
-    }
+    // Can be used for getting change or emptying machine stock
+    public int[] withdrawCash(double amount) {
+        int[] cashWithdraw = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        double origAmount = amount;
 
-    public void decreaseBal(double decBal) {
-        this.currentBal = currentBal - decBal;
+        // Removing cash from stock
+        int i=13;
+        while(i >= 0)
+            if(amount >= this.cashStock[0][i]) {
+                this.cashStock[1][i]--;
+                cashWithdraw[i]++;
+                amount -= cashStock[0][i];
+            } else i--;
+
+        if(amount != 0) {
+            // Returning extracted cash to stock if change is insufficient
+            for(i=0; i<14; i++)
+                this.cashStock[1][i] += cashWithdraw[i];
+            return null;
+        } else {
+            this.currentBal -= origAmount;
+            return cashWithdraw;
+        }
     }
 
     public double getCurrentBal() {
