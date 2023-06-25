90d73128aeb08117e867142d0fa05aa99a869e38
albarr15 <cmalbarracin15@gmail.com>
2023-06-25 12:29:22 +0800

    Removed receiveMoney, dispenseChange, and isValidDenomination in Transaction.java

--- src/main/java/regular_vm/Transaction.java
+++ src/main/java/regular_vm/Transaction.java
@@ -49,38 +49,4 @@ public class Transaction {
             return 1;
         }
     }
-
-
-    public void receiveMoney(Balance bal, double amount) {
-        if (isValidDenomination(amount)) {
-            this.amtReceived = this.amtReceived + amount;
-            bal.getListDenominations().add(amount);
-        }
-        else {
-            System.out.println("Error : Invalid Denomination");
-        }
-    }
-
-    public double dispenseChange(Balance bal) {
-        // to be modified,, use balance of regular vm for change and apply specific denominations
-
-        if (amtReceived < orderTotal) {
-            bal.decreaseBal(amtReceived);
-            return amtReceived;
-        }
-        else {
-            bal.decreaseBal(amtReceived - orderTotal);
-            return amtReceived - orderTotal;
-        }
-    }
-
-    public boolean isValidDenomination(double amount) {
-        if (amount == 1000 || amount == 500 || amount == 200 || amount == 100 || amount == 50 || amount == 20 ||
-            amount == 10 || amount == 5 || amount == 1 || amount == 0.25 || amount == 0.05 || amount == 0.01) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 }
