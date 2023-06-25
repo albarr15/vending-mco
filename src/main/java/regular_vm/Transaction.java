e4c9c584cbf9c04e5aa0cba554ab2e0ef8e7d7ee
albarr15 <cmalbarracin15@gmail.com>
2023-06-25 12:28:20 +0800

    Created checkOut method in Transaction.java

--- src/main/java/regular_vm/Transaction.java
+++ src/main/java/regular_vm/Transaction.java
@@ -1,5 +1,7 @@
 package regular_vm;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Character;
 
 public class Transaction {
     private ArrayList<Item> currentCart = new ArrayList<Item>();
@@ -24,6 +26,30 @@ public class Transaction {
         currentCart.remove(item);
         orderTotal = orderTotal + itemSlot.price;
     }
+
+    // shows summary of total cart and prompts for order placement
+    // returns 0 if successful, 1 if user wants to go back to modifying currentCart items
+    public int checkOut(Balance bal){
+        for(int i = 0; i < currentCart.size(); i++) {
+            System.out.println("[i] " + currentCart.get(i));
+        }
+        System.out.println("Place Order? (y/n)");
+        Scanner scanner = new Scanner(System.in);
+        char input = scanner.next().charAt(0);
+        if (input == 'y') {
+            // prompt for cash deposit
+            double initialBal = bal.getCurrentBal();
+            bal.depositCash(scanner.next());
+
+            // get newly added cash for this specific transaction
+            amtReceived = initialBal - bal.getCurrentBal();
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+
 
     public void receiveMoney(Balance bal, double amount) {
         if (isValidDenomination(amount)) {
