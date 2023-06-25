302f53331b5afdca82ed922c3056ce7856f8fb10
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-06-25 16:13:54 +0800

    Added price attribute for Item.java

--- src/main/java/regular_vm/Item.java
+++ src/main/java/regular_vm/Item.java
@@ -1,25 +1,37 @@
 package regular_vm;
 
 public class Item {
-    String name;
-    double caloriesAmt;
+    private String name;
+    private double caloriesAmt;
+    private double price;
 
-    public Item(String name, double caloriesAmt) {
+    public Item(String name, double caloriesAmt, double price) {
         this.name = name;
         this.caloriesAmt = caloriesAmt;
+        this.price = price;
     }
 
     public void setName(String name) {
         this.name = name;
     }
+    
     public void setCaloriesAmt(double caloriesAmt) {
         this.caloriesAmt = caloriesAmt;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public String getName() {
         return this.name;
     }
+    
     public double getCaloriesAmt() {
         return this.caloriesAmt;
+    }
+
+    public double getPrice() {
+        return this.price;
     }
 }
