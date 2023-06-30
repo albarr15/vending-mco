127389d5fcc364ace591944c8eaa2fdce114856d
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-06-30 13:13:23 +0800

    Added checkout validation for not enough change in machine and order total in preview for Transaction.java

--- src/main/java/regular_vm/Transaction.java
+++ src/main/java/regular_vm/Transaction.java
@@ -34,11 +34,13 @@ public class Transaction {
         for(int i = 0; i < currentCart.size(); i++) {
             System.out.println("[i] " + currentCart.get(i));
         }
+        System.out.println("Current total: " + orderTotal);
     }
 
     // returns 0 if successful, 1 if not
     public int checkOut(Balance bal){
         Scanner scanner = new Scanner(System.in);
+        String change;
 
         // prompt for cash deposit
         double initialBal = bal.getCurrentBal();
@@ -46,19 +48,30 @@ public class Transaction {
 
         // get newly added cash for this specific transaction
         this.amtReceived = initialBal - bal.getCurrentBal();
+        double changeAmt = this.amtReceived - this.orderTotal;
 
         if (this.amtReceived < this.orderTotal) {
             System.out.println("TRANSACTION UNSUCCESSFUL (Not enough cash entered)");
             System.out.println("Returning cash ...");
             bal.withdrawCash(this.amtReceived);
-
+            this.amtReceived = 0;
             return 1;
         }
-        else {
-            double changeAmt = this.amtReceived - orderTotal;
+        else if((change = bal.withdrawCash(changeAmt)) == null) {
+            System.out.println("TRANSACTION UNSUCCESSFUL (Not enough change in machine)");
+            System.out.println("Returning cash ...");
+            bal.withdrawCash(this.amtReceived);
+            this.amtReceived = 0;
+            return 1;
+        } else {
             System.out.println("TRANSACTION SUCCESSFUL");
             System.out.println("Withdrawing change ...");
-            bal.withdrawCash(changeAmt);
+            System.out.println("Your change is: " + change);
+            System.out.println("Dispensing " + this.currentCart.get(0).getName());
+            for(int i=0; i<currentCart.size(); i++) {
+                System.out.println(", " + currentCart.get(i).getName());
+            }
+            System.out.println(". . .");
             return 0;
         }
     }
