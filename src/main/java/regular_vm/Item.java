293aa814f6b576f59aa396fc0d56767dc3608762
albarr15 <cmalbarracin15@gmail.com>
2023-07-01 22:37:19 +0800

    Added javadoc comment for Item.java

--- src/main/java/regular_vm/Item.java
+++ src/main/java/regular_vm/Item.java
@@ -10,6 +10,12 @@ public class Item {
     private int caloriesAmt;
     private int price;
 
+    /**
+     * Creates an instance of an Item
+     * @param name is the item's name
+     * @param caloriesAmt is the item's calorie count
+     * @param price is the item's price
+     */
     public Item(String name, int caloriesAmt, int price) {
         this.name = name;
         this.caloriesAmt = caloriesAmt;
