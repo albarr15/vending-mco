cba8d9db7b84afa0df8f789f5557dbeaaa1c6950
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-07-01 17:14:05 +0800

    Added javadoc for ItemSlot.java

--- src/main/java/regular_vm/ItemSlot.java
+++ src/main/java/regular_vm/ItemSlot.java
@@ -1,3 +1,9 @@
+/**
+ * Represents one slot which holds Items in a vending machine.
+ * <p>
+ * Includes the Item set, the list of Items, the price, and the number sold.
+ * Features include checking availability, dispensing, and stocking items.
+ */
 package regular_vm;
 import java.util.ArrayList;
 
@@ -9,6 +15,11 @@ public class ItemSlot {
 
     public ItemSlot() {}
 
+    /**
+     * Sets the Item that this ItemSlot will contain.
+     * 
+     * @param item  the Item that will be set
+     */
     public void setItem(Item item) {
         this.item = item;
         listItem.clear();
@@ -16,31 +27,61 @@ public class ItemSlot {
         this.price = item.getPrice();
     }
 
+    /** 
+     * Returns the Item in this ItemSlot
+     * 
+     * @return  the Item in this ItemSlot
+     */
     public Item getItem() {
         return this.item;
     }
 
+    /** 
+     * Returns the name of the Item in this ItemSlot
+     * 
+     * @return  the name of Item in this ItemSlot
+     */
     public String getItemName() {
         return this.item.getName();
     }
 
+    /**
+     * Sets the price of the Items in this ItemSlot
+     * 
+     * @param price  the price that the Items will be set to
+     */
     public void setPrice(double price) {
         this.price = price;
 
         for (int i=0; i<this.listItem.size(); i++)
             this.listItem.get(i).setPrice(price);
     }
-    
+
+    /**
+     * Returns the price of the Items in this ItemSlot
+     * 
+     * @return  the price of the Items in this ItemSlot
+     */
     public double getPrice() {
         return this.price;
     }
 
+    /**
+     * Checks if there has been any Item set to this ItemSlot or if it is empty
+     * 
+     * @return  a boolean value of whether or not this slot is occupied
+     */
     public boolean checkSlotAvailability() {
         if(this.item == null || this.listItem.size() == 0)
             return true;
         else return false;
     }
 
+    /**
+     * Removes an Item from the list of Items stored in this ItemSlot
+     * 
+     * @return  the Item removed from this list
+     */
     public Item dispenseItem() {
         if(listItem.size() > 0) {
             Item item = this.listItem.get(this.listItem.size()-1);
@@ -50,6 +91,12 @@ public class ItemSlot {
         } else return null;
     }
 
+    /**
+     * Creates Item stock for this list of Items in this ItemSlot
+     * 
+     * @param isReturnedItem  tells if the item was returned from a buyer's cart rather than restocked
+     * @return  a boolean value of whether or not the stocking was successful
+     */
     public boolean stockItem(boolean isReturnedItem) {
         if(listItem.size() < 10) {
             Item item = new Item(this.item.getName(), 
@@ -62,6 +109,11 @@ public class ItemSlot {
         } else return false;
     }
 
+    /**
+     * Fully stocks an ItemSlot's list of Items
+     * 
+     * @return  a boolean value of whether or not there is room for restocking
+     */
     public boolean fullStockSlot() {
         if (listItem.size() == 10)
         {
@@ -75,10 +127,20 @@ public class ItemSlot {
         }
     }
 
+    /**
+     * Returns the number of Items currently stocked
+     * 
+     * @return  integer number of Items currently stocked in this ItemSlot
+     */
     public int getItemStock() {
         return this.listItem.size();
     }
 
+    /**
+     * Returns the number of Items that have been sold since the last restock
+     * 
+     * @return  integer number of sold items
+     */
     public int getNoSold() {
         return this.noSold;
     }
