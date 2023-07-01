2af9d3860779113334753dd44b357e34c347c06b
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-07-01 17:27:30 +0800

    Added javadoc for Maintenance.java

--- src/main/java/regular_vm/Maintenance.java
+++ src/main/java/regular_vm/Maintenance.java
@@ -1,3 +1,8 @@
+/** Contains the maintenance features of the vending machine.
+ * <p>
+ * Features include setting Items of ItemSlots, stocking/restocking Items, setting
+ * prices, collecting money from the Balance, and printing the summary of transactions.
+ */
 package regular_vm;
 import java.util.ArrayList;
 
@@ -8,12 +13,26 @@ public class Maintenance {
         this.currentVM = currentVM;
     }
 
+    /**
+     * Creates a new Item to be set as the Item of an ItemSlot
+     * 
+     * @param itemSlot  the slot for the Item to be placed in
+     * @param itemName  the name of the Item
+     * @param caloriesAmt  the calorie count of the Item
+     * @param price  the price of the Item
+     */
     public void setItem(ItemSlot itemSlot, String itemName,
                         double caloriesAmt, double price) {
         Item item = new Item(itemName, caloriesAmt, price);
         itemSlot.setItem(item);
     }
 
+    /**
+     * Stocks an ItemSlot of a certain amoutn of whatever Item is assigned to it
+     * 
+     * @param itemSlot  the ItemSlot to be stocked
+     * @param numItems  the number of Items to be stocked
+     */
     public void stockItem(ItemSlot itemSlot, int numItems) {
         int i=0;
         for(i=0; i < numItems; i++)
@@ -28,31 +47,68 @@ public class Maintenance {
                                " " + itemSlot.getItemName() + ".");
     }
 
+    /**
+     * Fully stocks an ItemSlot of whatever Item is assigned to it
+     * 
+     * @param itemSlot  the ItemSlot to be stocked
+     */
     public void stockItem(ItemSlot itemSlot) {
         itemSlot.fullStockSlot();
         System.out.println("Fully stocked " + itemSlot.getItemName() + ".");
     }
 
+    /**
+     * Sets the price of Items in an ItemSlot
+     * 
+     * @param itemSlot  the ItemSlot to be given a price
+     * @param price  the price to be set
+     */
     public void setPrice(ItemSlot itemSlot, double price) {
         itemSlot.setPrice(price);
     }
 
+    /**
+     * Collects a certain amount of money from the machine
+     * 
+     * @param bal  the Balance of the machine
+     * @param amount  the amount to be collected
+     * @return  the remaining balance or null if unsuccessful
+     */
     public double collectMoney(Balance bal, double amount) {
         if(bal.withdrawCash(amount) != null)
             return bal.getCurrentBal();
         else return 0;
     }
+
+    /**
+     * Collects all the money from the machine
+     * 
+     * @param bal  the Balance of the machine
+     * @return  the remaining balance
+     */
     public double collectMoney(Balance bal) {
         double origBal = bal.getCurrentBal();
         bal.withdrawCash(bal.getCurrentBal());
         return origBal;
     }
 
+    /**
+     * Replenishes the stock of bills and coins in the machine
+     * 
+     * @param bal  the Balance of the machine
+     * @param cashList  a String containing values of the money to be stocked
+     * @return  the new balance after stocking
+     */
     public double replenishMoney(Balance bal, String cashList) {
         bal.depositCash(cashList);
         return bal.getCurrentBal();
     }
 
+    /**
+     * Prints the summary of transactions since the last summary printed
+     * 
+     * @param slots  the list of ItemSlots in the machine
+     */
     public void printTransacSummary(ArrayList<ItemSlot> listItemSlots) {
         double totalEarnings = 0;
         System.out.println("Number of items sold: ");
@@ -67,5 +123,4 @@ public class Maintenance {
         }
         System.out.println("Total earnings since last summary: " + totalEarnings);
     }
-
 }
