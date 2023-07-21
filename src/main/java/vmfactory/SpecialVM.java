a850352808071e02e2f29624e8ce8e8fb173e002
t-akoyaki <119575104+t-akoyaki@users.noreply.github.com>
2023-07-21 20:08:48 +0800

    Added displaySaleSlots()

--- src/main/java/vmfactory/SpecialVM.java
+++ src/main/java/vmfactory/SpecialVM.java
@@ -9,4 +9,28 @@ public class SpecialVM extends VendingMachine {
     public void makeTransaction() {
         this.currentTransaction = new SpecialTransaction();
     }
+
+    public void displaySaleSlots() {
+        int size = listItemSlots.size();
+
+        // prints each itemSlot with its corresponding stocked item
+        for (int i = 0; i < size; i++) {
+            if(listItemSlots.get(i).getForSale()) {
+                if ((listItemSlots.get(i)).checkSlotAvailability()) {
+                    System.out.println("Slot " + (i + 1) + " ( OUT OF STOCK )");
+                }
+                else {
+                    System.out.printf("%-5s", "[" + listItemSlots.get(i).getItemStock() + "]");
+                    System.out.printf("%-10s", "Slot " + (i + 1));
+                    System.out.printf("%-20s", "(" + (listItemSlots.get(i)).getItemName() + ")");
+                    System.out.printf("%-15s", "Price : " + listItemSlots.get(i).getPrice());
+                    System.out.printf("%-12s", "Calories : " + listItemSlots.get(i).getItem().getCaloriesAmt());
+                    if(listItemSlots.get(i).getItem() instanceof SpecialItem)
+                        System.out.printf("\t  (Special)");
+                    System.out.println();
+                }
+            }
+            
+        }
+    }
 }
