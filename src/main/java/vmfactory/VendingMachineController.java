83e5cb9470c5bb131dc1880fb0ecd26d93fd886e
albarr15 <cmalbarracin15@gmail.com>
2023-07-24 10:38:36 +0800

    Added initial code for VendingMachineController.java

---
+++ src/main/java/vmfactory/VendingMachineController.java
@@ -0,0 +1,10 @@
+package vmfactory;
+
+public class VendingMachineController {
+    private VendingMachineView vmView;
+    private VendingMachine vmModel;
+
+    public VendingMachineController (VendingMachineView vmView, VendingMachine vmModel) {
+
+    }
+}
