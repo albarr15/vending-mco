d60ca1bd2af416e036152f2f659646f064009ac4
albarr15 <cmalbarracin15@gmail.com>
2023-07-24 11:34:05 +0800

    Created initial code for Main class

---
+++ src/main/java/vmfactory/Main.java
@@ -0,0 +1,11 @@
+package vmfactory;
+
+public class Main {
+    public static void main(String[] args) {
+        VMFactoryView vmFactoryView = new VMFactoryView();
+        VMFactoryModel vmFactoryModel = new VMFactoryModel();
+        VMFactoryController vmFactoryController = new VMFactoryController(vmFactoryView, vmFactoryModel);
+
+
+    }
+}
