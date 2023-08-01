9274b05167eac5bf57742905a57648cbfc8a0f39
albarr15 <cmalbarracin15@gmail.com>
2023-08-01 11:42:27 +0800

    Added javadoc in SpecialItem.java

--- src/main/java/vmfactory/SpecialItem.java
+++ src/main/java/vmfactory/SpecialItem.java
@@ -2,6 +2,10 @@ package vmfactory;
 
 import java.util.ArrayList;
 
+/** Represents a special Item to be stored in a Special Vending Machine
+ * <p>
+ * Features include adding and removing a component, and printing preparation message
+ */
 public class SpecialItem extends Item{
     private ArrayList<Item> listComponents = new ArrayList<Item>();
 
@@ -13,12 +17,20 @@ public class SpecialItem extends Item{
         return this.listComponents;
     }
 
+    /**
+     * Adds a component to the special item's list of components
+     * @param item is the item to be added onto the list of components
+     */
     public void addComponent(Item item) {
         listComponents.add(item);
         this.setCaloriesAmt(this.getCaloriesAmt() + item.getCaloriesAmt());
         this.setPrice(this.getPrice() + item.getPrice());
     }
 
+    /**
+     * Removes a component from the special item's list of components
+     * @param itemName is the item to be removed from the existing list of components
+     */
     public Item removeComponent(String itemName) {
         Item removedItem = null;
 
@@ -36,6 +48,9 @@ public class SpecialItem extends Item{
         return removedItem;
     }
 
+    /**
+     * Prints the preparation message for each of the components in the list of Components
+     */
     public void printPreparation() {
         for (Item listComponent : listComponents) {
             System.out.println(listComponent.getPrepMessage());
